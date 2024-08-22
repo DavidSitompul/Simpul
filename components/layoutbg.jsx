d3cbbe54ea476be036b064aa453ded30c6e8b563
DavidSitompul <dpaulsitompul@gmail.com>
2024-08-22 22:47:24 +0700

    delete bg

--- components/layoutbg.jsx
+++ components/layoutbg.jsx
@@ -6,7 +6,7 @@ const LayoutBg = () => {
   return (
     <div className="bg-[#333333] min-h-screen w-full flex">
       <div className="w-[285px] border-r-[1px] border-[#F2F2F2]">side skirsi</div>
-      <div className="bg-red-500 w-full">
+      <div className=" w-full">
         <div className="w-full bg-[#4F4F4F] h-[58px] pt-[19px] pl-6 pb-[23px]">
           <IoIosSearch color="#F2F2F2" size={16} />
         </div>
